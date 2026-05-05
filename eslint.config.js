b3e3801541e9e07f4ddfc44ae1223cb93e8ef838
Cong Nguyen <ankerek@gmail.com>
2026-05-05 09:55:35 +0200

    chore: rename app.json identity to Trip Pocket; trim redundant eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,12 @@
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 const prettierConfig = require('eslint-config-prettier');
-const tsPlugin = require('@typescript-eslint/eslint-plugin');
-const tsParser = require('@typescript-eslint/parser');
 
 module.exports = defineConfig([
   ...expoConfig,
   prettierConfig,
   {
     files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: tsParser,
-    },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-    },
     rules: {
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
